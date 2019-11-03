dc9d45d71a4f6b753645079e3b2b304009eb2b21
gretzky <sfed617@gmail.com>
2019-11-03 16:41:43 -0500

    [teams] add logos

--- packages/teams/src/teams.ts
+++ packages/teams/src/teams.ts
@@ -4,105 +4,135 @@ export default [
     name: "NEW JERSEY DEVILS",
     abbrev: "NJD",
     nicknames: ["DEVS"],
-    colors: ["#F47A38", "#B9975C", "#C1C6C8", "#000"]
+    colors: ["#F47A38", "#B9975C", "#C1C6C8", "#000"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/New_Jersey_Devils_logo.svg/594px-New_Jersey_Devils_logo.svg.png"
   },
   {
     id: 2,
     name: "NEW YORK ISLANDERS",
     abbrev: "NYI",
     nicknames: ["ISLES", "FISHSTICKS", "FISH STICKS"],
-    colors: ["#00539B", "#F47D30"]
+    colors: ["#00539B", "#F47D30"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Logo_New_York_Islanders.svg/440px-Logo_New_York_Islanders.svg.png"
   },
   {
     id: 3,
     name: "NEW YORK RANGERS",
     abbrev: "NYR",
     nicknames: ["BLUESHIRTS", "RAGS"],
-    colors: ["#0038A8", "#C31126", "#FFF"]
+    colors: ["#0038A8", "#C31126", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/New_York_Rangers.svg/440px-New_York_Rangers.svg.png"
   },
   {
     id: 4,
     name: "PHILADELPHIA FLYERS",
     abbrev: "PHI",
     nicknames: ["BROAD ST. BULLIES"],
-    colors: ["#F74902", "#000", "#FFF"]
+    colors: ["#F74902", "#000", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/Philadelphia_Flyers.svg/440px-Philadelphia_Flyers.svg.png"
   },
   {
     id: 5,
     name: "PITTSBURGH PENGUINS",
     abbrev: "PIT",
     nicknames: ["SHITTSBURGH", "PENS"],
-    colors: ["#000", "#CFC493", "#FCB514", "#FFF"]
+    colors: ["#000", "#CFC493", "#FCB514", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/Pittsburgh_Penguins_logo_%282016%29.svg/440px-Pittsburgh_Penguins_logo_%282016%29.svg.png"
   },
   {
     id: 6,
     name: "BOSTON BRUINS",
     abbrev: "BOS",
     nicknames: ["B'S", "BIG BAD BRUINS", "BLACK AND GOLD"],
-    colors: ["#000", "#FFB81C"]
+    colors: ["#000", "#FFB81C"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Boston_Bruins.svg/440px-Boston_Bruins.svg.png"
   },
   {
     id: 7,
     name: "BUFFALO SABRES",
     abbrev: "BUF",
     nicknames: ["SWORDS", "SLUGS"],
-    colors: ["#002654", "#FCB514", "#ADAFAA", "#C8102E"]
+    colors: ["#002654", "#FCB514", "#ADAFAA", "#C8102E"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Buffalo_Sabres_Logo.svg/440px-Buffalo_Sabres_Logo.svg.png"
   },
   {
     id: 8,
     name: "MONTREAL CANADIENS",
     abbrev: "MTL",
     nicknames: ["HABS", "HABITANTS", "BLEU-BLANC-ROUGE", "TOILET BOWLS"],
-    colors: ["#AF1E2D", "#192168"]
+    colors: ["#AF1E2D", "#192168"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Montreal_Canadiens.svg/440px-Montreal_Canadiens.svg.png"
   },
   {
     id: 9,
     name: "OTTAWA SENATORS",
     abbrev: "OTT",
     nicknames: ["SENS"],
-    colors: ["#C52032", "#C2912C", "#000", "#FFF"]
+    colors: ["#C52032", "#C2912C", "#000", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Ottawa_Senators.svg/440px-Ottawa_Senators.svg.png"
   },
   {
     id: 10,
     name: "TORONTO MAPLE LEAFS",
     abbrev: "TOR",
     nicknames: ["LEAFS", "CRY BABIES"],
-    colors: ["#00205B", "#FFF"]
+    colors: ["#00205B", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Toronto_Maple_Leafs_2016_logo.svg/440px-Toronto_Maple_Leafs_2016_logo.svg.png"
   },
   {
     id: 11,
     name: "ATLANTA THRASHERS",
     abbrev: "ATL",
     nicknames: ["THRASH"],
-    colors: ["#041E42", "#5C88DA", "#862633", "#B76125", "#FFA300", "#FFF"]
+    colors: ["#041E42", "#5C88DA", "#862633", "#B76125", "#FFA300", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Atlanta_Thrashers.svg/440px-Atlanta_Thrashers.svg.png"
   },
   {
     id: 12,
     name: "CAROLINA HURRICANES",
     abbrev: "CAR",
     nicknames: ["CANES", "JERKS"],
-    colors: ["#CC0000", "#000", "#A2AAAD", "#76232F"]
+    colors: ["#CC0000", "#000", "#A2AAAD", "#76232F"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Carolina_Hurricanes.svg/440px-Carolina_Hurricanes.svg.png"
   },
   {
     id: 13,
     name: "FLORIDA PANTHERS",
     abbrev: "FLA",
     nicknames: ["CATS"],
-    colors: ["#041E42", "#C8102E", "#B9975B"]
+    colors: ["#041E42", "#C8102E", "#B9975B"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Florida_Panthers_2016_logo.svg/440px-Florida_Panthers_2016_logo.svg.png"
   },
   {
     id: 14,
     name: "TAMPA BAY LIGHTNING",
     abbrev: "TBL",
     nicknames: ["BOLTS"],
-    colors: ["#002868", "#FFF"]
+    colors: ["#002868", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/Tampa_Bay_Lightning_Logo_2011.svg/440px-Tampa_Bay_Lightning_Logo_2011.svg.png"
   },
   {
     id: 15,
     name: "WASHINGTON CAPITALS",
     abbrev: "WSH",
     nicknames: ["CAPS", "SECOND ROUND EXIT"],
-    colors: ["#041E42", "#C8102E", "#FFF"]
+    colors: ["#041E42", "#C8102E", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Washington_Capitals.svg/440px-Washington_Capitals.svg.png"
   },
   {
     id: 16,
@@ -118,56 +148,72 @@ export default [
       "#D18A00",
       "#001970",
       "#FFF"
-    ]
+    ],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Chicago_Blackhawks_logo.svg/440px-Chicago_Blackhawks_logo.svg.png"
   },
   {
     id: 17,
     name: "DETROIT RED WINGS",
     abbrev: "DET",
     nicknames: ["DEAD WINGS", "WINGS"],
-    colors: ["#CE1126", "#FFF"]
+    colors: ["#CE1126", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Detroit_Red_Wings_logo.svg/440px-Detroit_Red_Wings_logo.svg.png"
   },
   {
     id: 18,
     name: "NASHVILLE PREDATORS",
     abbrev: "NSH",
     nicknames: ["PREDS", "SMASHVILLE"],
-    colors: ["#FFB81C", "#041E42", "#FFF"]
+    colors: ["#FFB81C", "#041E42", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Nashville_Predators_Logo_%282011%29.svg/440px-Nashville_Predators_Logo_%282011%29.svg.png"
   },
   {
     id: 19,
     name: "ST. LOUIS BLUES",
     abbrev: "STL",
     nicknames: ["NOTES", "THE NOTES"],
-    colors: ["#002F87", "#FCB514", "#041E42", "#FFF"]
+    colors: ["#002F87", "#FCB514", "#041E42", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/St._Louis_Blues_logo.svg/440px-St._Louis_Blues_logo.svg.png"
   },
   {
     id: 20,
     name: "CALGARY FLAMES",
     abbrev: "CGY",
     nicknames: ["FLAMING SNOT DONKEYS", "FLAMING C'S"],
-    colors: ["#C8102E", "#F1BE48", "#111", "#FFF"]
+    colors: ["#C8102E", "#F1BE48", "#111", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Calgary_Flames_Logo.svg/440px-Calgary_Flames_Logo.svg.png"
   },
   {
     id: 21,
     name: "COLORADO AVALANCHE",
     abbrev: "COL",
     nicknames: ["AVS"],
-    colors: ["#6F263D", "#236192", "#A2AAAD", "#000"]
+    colors: ["#6F263D", "#236192", "#A2AAAD", "#000"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Colorado_Avalanche_logo.svg/440px-Colorado_Avalanche_logo.svg.png"
   },
   {
     id: 22,
     name: "EDMONTON OILERS",
     abbrev: "EDM",
     nicknames: ["SPOILERS"],
-    colors: ["#041E42", "#FF4C00"]
+    colors: ["#041E42", "#FF4C00"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/Logo_Edmonton_Oilers.svg/440px-Logo_Edmonton_Oilers.svg.png"
   },
   {
     id: 23,
     name: "VANCOUVER CANUCKS",
     abbrev: "VAN",
     nicknames: ["NUCKS", "CANUCKLEHEADS"],
-    colors: ["#00205B", "#00843D", "#041C2C", "#99999A", "#FFF"]
+    colors: ["#00205B", "#00843D", "#041C2C", "#99999A", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/Vancouver_Canucks_logo.svg/440px-Vancouver_Canucks_logo.svg.png"
   },
   {
     id: 24,
@@ -183,21 +229,27 @@ export default [
       "#532A44",
       "#FCC72C",
       "#241E34"
-    ]
+    ],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/72/Anaheim_Ducks.svg/440px-Anaheim_Ducks.svg.png"
   },
   {
     id: 25,
     name: "DALLAS STARS",
     abbrev: "DAL",
     nicknames: ["SHERIFFS"],
-    colors: ["#006847", "#8F8F8C", "#111"]
+    colors: ["#006847", "#8F8F8C", "#111"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Dallas_Stars_logo_%282013%29.svg/440px-Dallas_Stars_logo_%282013%29.svg.png"
   },
   {
     id: 26,
     name: "LOS ANGELES KINGS",
     abbrev: "LAK",
     nicknames: ["MONARCHS"],
-    colors: ["#111", "#A2AAAD", "#FFF", "#572A84", "#FFC80C"]
+    colors: ["#111", "#A2AAAD", "#FFF", "#572A84", "#FFC80C"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Los_Angeles_Kings_logo.svg/440px-Los_Angeles_Kings_logo.svg.png"
   },
   {
     id: 53,
@@ -212,35 +264,45 @@ export default [
       "#DDCBA4",
       "#A9431E",
       "#5F259F"
-    ]
+    ],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/2/27/Arizona_Coyotes.svg/440px-Arizona_Coyotes.svg.png"
   },
   {
     id: 28,
     name: "SAN JOSE SHARKS",
     abbrev: "SJS",
     nicknames: ["FINS", "TUNA", "TEAM TEAL"],
-    colors: ["#006D75", "#000", "#EA7200", "#FFF"]
+    colors: ["#006D75", "#000", "#EA7200", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/SanJoseSharksLogo.svg/440px-SanJoseSharksLogo.svg.png"
   },
   {
     id: 29,
     name: "COLUMBUS BLUE JACKETS",
     abbrev: "CBJ",
     nicknames: ["JACKETS", "CANNONS"],
-    colors: ["#002654", "#CE1126", "#A4A9AD"]
+    colors: ["#002654", "#CE1126", "#A4A9AD"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Columbus_Blue_Jackets_logo.svg/440px-Columbus_Blue_Jackets_logo.svg.png"
   },
   {
     id: 30,
     name: "MINNESOTA WILD",
     abbrev: "MIN",
     nicknames: ["MINNY", "MILD", "MANBEARPIGS"],
-    colors: ["#A6192E", "#154734", "#EAAA00", "#DDCBA4"]
+    colors: ["#A6192E", "#154734", "#EAAA00", "#DDCBA4"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Minnesota_Wild.svg/440px-Minnesota_Wild.svg.png"
   },
   {
     id: 54,
     name: "VEGAS GOLDEN KNIGHTS",
     abbrev: "VGK",
     nicknames: ["GOLDEN MISFITS", "KNIGHTS"],
-    colors: ["#B4975A", "#333F42", "#C8102E", "#000", "#FFF"]
+    colors: ["#B4975A", "#333F42", "#C8102E", "#000", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/Vegas_Golden_Knights_logo.svg/440px-Vegas_Golden_Knights_logo.svg.png"
   },
   {
     id: 52,
@@ -255,49 +317,63 @@ export default [
       "#55565A",
       "#8E9090",
       "#FFF"
-    ]
+    ],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Winnipeg_Jets_Logo_2011.svg/440px-Winnipeg_Jets_Logo_2011.svg.png"
   },
   {
     id: 38,
     name: "PITTSBURGH PIRATES",
     abbrev: "PIR",
     nicknames: [],
-    colors: ["#000", "#FCB932", "#FFF", "#F87A36"]
+    colors: ["#000", "#FCB932", "#FFF", "#F87A36"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/8/85/Pittsburgh_Pirates_Logo.svg/440px-Pittsburgh_Pirates_Logo.svg.png"
   },
   {
     id: 58,
     name: "TORONTO ST. PATRICKS",
     abbrev: "TSP",
     nicknames: [],
-    colors: ["#046A38", "#FFF"]
+    colors: ["#046A38", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Toronto_St._Patricks_logo.svg/440px-Toronto_St._Patricks_logo.svg.png"
   },
   {
     id: 49,
     name: "CLEVELAND BARONS",
     abbrev: "CLE",
     nicknames: [],
-    colors: ["#C8102E", "#000", "#FFF"]
+    colors: ["#C8102E", "#000", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Cleveland_Barons_%28NHL%29_logo.svg/440px-Cleveland_Barons_%28NHL%29_logo.svg.png"
   },
   {
     id: 46,
     name: "OAKLAND SEALS",
     abbrev: "OAK",
     nicknames: [],
-    colors: ["#046A38", "#0033A0", "#FFF", "#FFC72C", "#009CA6"]
+    colors: ["#046A38", "#0033A0", "#FFF", "#FFC72C", "#009CA6"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/California_Golden_Seals_Logo.svg/440px-California_Golden_Seals_Logo.svg.png"
   },
   {
     id: 44,
     name: "NEW YORK AMERICANS",
     abbrev: "NYA",
     nicknames: [],
-    colors: ["#0038A8", "#C31126", "#FFF"]
+    colors: ["#0038A8", "#C31126", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/da/New_York_Americans_Logo.svg/440px-New_York_Americans_Logo.svg.png"
   },
   {
     id: 56,
     name: "CALIFORNIA GOLDEN SEALS",
     abbrev: "CGS",
     nicknames: [],
-    colors: ["#046A38", "#0033A0", "#FFF", "#FFC72C", "#009CA6"]
+    colors: ["#046A38", "#0033A0", "#FFF", "#FFC72C", "#009CA6"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/17/California_Golden_Seals_Logo.svg/440px-California_Golden_Seals_Logo.svg.png"
   },
   {
     id: 34,
@@ -313,90 +389,115 @@ export default [
       "#8D9093",
       "#000",
       "#FFF"
-    ]
+    ],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Hartford_Whalers_Logo-79-92.svg/440px-Hartford_Whalers_Logo-79-92.svg.png"
   },
   {
     id: 47,
     name: "ATLANTA FLAMES",
     abbrev: "AFM",
     nicknames: [],
-    colors: ["#C52032", "#C2912C", "#FFF"]
+    colors: ["#C52032", "#C2912C", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Atlanta_Flames_Logo.svg/440px-Atlanta_Flames_Logo.svg.png"
   },
   {
     id: 31,
     name: "MINNESOTA NORTH STARS",
     abbrev: "MNS",
     nicknames: [],
-    colors: ["#009639", "#FFD100", "#000", "#FFF"]
+    colors: ["#009639", "#FFD100", "#000", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Minnesota_North_Stars_Logo_2.svg/440px-Minnesota_North_Stars_Logo_2.svg.png"
   },
   {
     id: 32,
     name: "QUEBEC NORDIQUES",
     abbrev: "QUE",
     nicknames: [],
-    colors: ["#005EBB", "#DA291C", "#FFF"]
+    colors: ["#005EBB", "#DA291C", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/96/Quebec_Nordiques_Logo.svg/440px-Quebec_Nordiques_Logo.svg.png"
   },
   {
     id: 37,
     name: "HAMILTON TIGERS",
     abbrev: "HAM",
     nicknames: [],
-    colors: ["#000", "#FFC727", "#FFF"]
+    colors: ["#000", "#FFC727", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Hamilton_Tigers_Logo.svg/440px-Hamilton_Tigers_Logo.svg.png"
   },
   {
     id: 41,
     name: "MONTREAL WANDERERS",
     abbrev: "MWN",
     nicknames: [],
-    colors: ["#FFF", "#E4002B"]
+    colors: ["#FFF", "#E4002B"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Montreal_Wanderers_logo.svg/440px-Montreal_Wanderers_logo.svg.png"
   },
   {
     id: 42,
     name: "QUEBEC BULLDOGS",
     abbrev: "QBD",
     nicknames: [],
-    colors: ["#3E5EFF", "#FFF"]
+    colors: ["#3E5EFF", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/7/75/QuebecBulldogs.png"
   },
   {
     id: 57,
     name: "TORONTO ARENAS",
     abbrev: "TAN",
     nicknames: [],
-    colors: ["#003087"]
+    colors: ["#003087"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Toronto_Arenas_Logo.svg/440px-Toronto_Arenas_Logo.svg.png"
   },
   {
     id: 39,
     name: "PHILADELPHIA QUAKERS",
     abbrev: "QUA",
     nicknames: [],
-    colors: ["#000", "#F87A36", "#FFF"]
+    colors: ["#000", "#F87A36", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Logo_Philadelphia_Quakers.svg/440px-Logo_Philadelphia_Quakers.svg.png"
   },
   {
     id: 48,
     name: "KANSAS CITY SCOUTS",
     abbrev: "KCS",
     nicknames: [],
-    colors: ["#003087", "#C8102e", "#FFC72C", "#FFF"]
+    colors: ["#003087", "#C8102e", "#FFC72C", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Kansas_City_Scouts_logo.svg/440px-Kansas_City_Scouts_logo.svg.png"
   },
   {
     id: 45,
     name: "ST. LOUIS EAGLES",
     abbrev: "SLE",
     nicknames: [],
-    colors: ["#C60C30", "#FFF"]
+    colors: ["#C60C30", "#FFF"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/St._Louis_Eagles_logo.svg/440px-St._Louis_Eagles_logo.svg.png"
   },
   {
     id: 43,
     name: "MONTREAL MAROONS",
     abbrev: "MMR",
     nicknames: [],
-    colors: ["#FFF", "#6F263D"]
+    colors: ["#FFF", "#6F263D"],
+    logo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Montreal_Maroons_Logo.svg/440px-Montreal_Maroons_Logo.svg.png"
   },
   {
     id: 50,
     name: "DETROIT FALCONS",
     abbrev: "DFL",
     nicknames: [],
-    colors: ["#CE1126", "#FFF"]
+    colors: ["#CE1126", "#FFF"],
+    logo: ""
   }
 ];
